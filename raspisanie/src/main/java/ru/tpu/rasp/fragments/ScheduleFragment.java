79f72f9ef950fd184c8befe91cb70e4baea9c290
Ekzee <avpogrebnoy@gmail.com>
2014-03-16 17:12:21 +0700

    initial commit

---
+++ raspisanie/src/main/java/ru/tpu/rasp/fragments/ScheduleFragment.java
@@ -0,0 +1,15 @@
+package ru.tpu.rasp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Andrey on 16.03.14.
+ */
+public class ScheduleFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+}

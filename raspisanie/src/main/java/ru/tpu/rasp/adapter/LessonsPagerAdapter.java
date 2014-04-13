57fee4aa3d42271d8c92d49640df3c64ea9c779d
Ekzee <avpogrebnoy@gmail.com>
2014-04-13 17:01:53 +0700

    пустой список

--- raspisanie/src/main/java/ru/tpu/rasp/adapter/LessonsPagerAdapter.java
+++ raspisanie/src/main/java/ru/tpu/rasp/adapter/LessonsPagerAdapter.java
@@ -1,8 +1,10 @@
 package ru.tpu.rasp.adapter;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.view.PagerAdapter;
 import android.util.SparseArray;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -16,6 +18,7 @@ import ru.tpu.rasp.data.WeekSchedule;
 public class LessonsPagerAdapter extends PagerAdapter {
 
 	private Context mContext;
+	private LayoutInflater mInflater;
 	private String[] mDaysOfWeek;
 	private WeekSchedule mWeekSchedule;
 	private SparseArray<ListView> mLists = new SparseArray<ListView>();
@@ -23,21 +26,29 @@ public class LessonsPagerAdapter extends PagerAdapter {
 
 	public LessonsPagerAdapter(Context context) {
 		mContext = context;
+		mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		mDaysOfWeek = context.getResources().getStringArray(R.array.daysOfWeek);
 	}
 
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
-		ListView view = new ListView(mContext);
+		View v = mInflater.inflate(R.layout.item_lessons_list, container, false);
+		ListView listView = (ListView) v.findViewById(android.R.id.list);
+		View emptyView = v.findViewById(android.R.id.empty);
 		LessonsAdapter lessonsAdapter = new LessonsAdapter(mContext);
+
+		listView.setEmptyView(emptyView);
+		listView.setAdapter(lessonsAdapter);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+			listView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+		}
 		if (mWeekSchedule != null){
 			lessonsAdapter.setLessons(mWeekSchedule.getDayLessons(position));
 		}
-		view.setAdapter(lessonsAdapter);
-		container.addView(view);
-		mLists.put(position, view);
+		container.addView(v);
+		mLists.put(position, listView);
 		mAdapters.put(position, lessonsAdapter);
-		return view;
+		return v;
 	}
 
 	@Override
